cd515443ae6d2806c48b9fcee6a8218b1d5eb1a0
JPaulinoN <44654342+JPaulinoN@users.noreply.github.com>
2018-10-31 17:24:21 -0400

    Add files via upload

---
+++ OdeToFood/OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    //about
+    [Route("company/[controller]/[action]")]
+    public class AboutController
+    {
+        public string Phone()
+        {
+            return "69-99205-5302";
+        }
+        public string Address()
+        {
+            return "Brasil";
+        }
+    }
+}
